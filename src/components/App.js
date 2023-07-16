2c9aca33aed76bb0d67ffdcf054c7999b4b7afe7
Rahul Jadhav <jadhav.rj.cmos@gmail.com>
2023-07-16 19:07:55 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,38 @@
 import React, { useState } from 'react'
 import '../styles/App.css';
 
-function ToDo() {
+function ToDo({id,task,createdAt}) {
   return (<tr>
     <td>
       <p>id</p>
+        {id}
     </td>
     <td>
       <input />
+      {task}
     </td>
     <td>
       <p>createdAt</p>
+      {createdAt}
     </td>
   </tr>)
 }
 
 function App() {
-  const [todos, setTodos] = useState([{
-    id: 'todo1',
-    createdAt: '20:30',
-  }, {
-    id: 'todo2',
-    createdAt: '18:00',
-  }
+  const [todos, setTodos] = useState([
+    { id: 'todo1',task:'Task 1',createdAt: '2022-01-01' },
+    { id: 'todo2',task:'Task 2',createdAt: '2022-02-02' },
+    { id: 'todo3',task:'Task 3',createdAt: '2022-03-03' },
   ]);
-
+  const reverseOrder=()=>{
+    setTodos(prevTodos =>[...prevTodos].revers());
+  };
   return (
     <div id="main">
-      <button>Reverse</button>
+      <button onClick={reverseOrder}>Reverse</button>
       <table>
         <tbody>
+          <ToDO key={todo.id} task={todo.task} createdAt={todo.createdAt} />
         </tbody>
       </table>
     </div>
